be90a9165032a52229811ad986dbceb1e4c002a6
Darlan Alves <me@darlanalv.es>
2019-08-12 01:12:36 +0200

    chore: initialise repo

---
+++ README.md
@@ -0,0 +1,8 @@
+## Betty
+
+Put your ESP8266 in flash mode and run the following im a terminal:
+
+```
+make build && make flash
+```
+
---
+++ src/index.cpp
@@ -0,0 +1,55 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "index.h"
+#include "wifi.h"
+#include "ntp.h"
+#include "websocket.h"
+#include "homebots.h"
+
+static os_timer_t webSocketCheck;
+static Wifi wifiConnection;
+static ws_info webSocket;
+
+void ICACHE_FLASH_ATTR onReceive(struct ws_info *wsInfo, int length, char *message, int opCode) {
+  switch (opCode) {
+    case WS_OPCODE_BINARY:
+      break;
+
+    case WS_OPCODE_TEXT:
+      os_printf("%d %s\n", length, message);
+
+
+      ws_send(wsInfo, WS_OPCODE_TEXT, message, length);
+      break;
+  }
+}
+
+void ICACHE_FLASH_ATTR loop() {}
+
+void ICACHE_FLASH_ATTR connectWebSocket() {
+  if (!wifiConnection.isConnected()) {
+    wifiConnection.printStatus();
+    return;
+  }
+
+  if (webSocket.connectionState != CS_CONNECTED) {
+    ws_connect(&webSocket, "ws://hub.homebots.io/hub");
+    return;
+  }
+
+  os_timer_arm(&webSocketCheck, 5000, 1);
+}
+
+void ICACHE_FLASH_ATTR setup() {
+  os_timer_setfn(&webSocketCheck, (os_timer_func_t *)connectWebSocket, NULL);
+  os_timer_arm(&webSocketCheck, 1000, 1);
+
+  wifiConnection.connectTo("HomeBots", "HomeBots");
+  webSocket.onReceive = onReceive;
+}
+
+#ifdef __cplusplus
+}
+#endif
